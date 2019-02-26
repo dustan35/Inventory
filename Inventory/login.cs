4d75d80981d61b16f5b0e837aaf3ed689e677b30
Dustan Moore <dustan26@live.com>
2019-02-26 13:03:00 -0600

    push

--- Inventory/login.cs
+++ Inventory/login.cs
@@ -1,19 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Input;
 using System.Data.SqlClient;
-using System.Diagnostics;
-using System.Threading;
-using System.Windows.Controls;
-using System.Data.OleDb;
-using System.IO;
 
 namespace InventoryMB
 {
